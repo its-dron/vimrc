cd86bbb5467efd39dcb95e6505a00acb2906d45f
amix <amix@amix.dk>
2017-12-13 15:05:24 +0100

    Replace YanRing with yank-stack and update plugins

--- update_plugins.py
+++ update_plugins.py
@@ -16,7 +16,7 @@ from os import path
 
 #--- Globals ----------------------------------------------
 PLUGINS = """
-YankRing.vim https://github.com/vim-scripts/YankRing.vim
+vim-yankstack https://github.com/maxbrunsfeld/vim-yankstack
 ack.vim https://github.com/mileszs/ack.vim
 bufexplorer https://github.com/corntrace/bufexplorer
 ctrlp.vim https://github.com/ctrlpvim/ctrlp.vim
